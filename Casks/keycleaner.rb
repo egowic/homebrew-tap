7ccf34998b274829df1c3e7c9e7bf08b322b32f4
Ege Bilir <ege.bilir@gmail.com>
2026-04-25 17:11:21 +0300

    keycleaner 1.3.1

--- Casks/keycleaner.rb
+++ Casks/keycleaner.rb
@@ -1,6 +1,6 @@
 cask "keycleaner" do
-  version "1.3.0"
-  sha256 "49c65be5f82b8447fa83e478c27210da05ffdbcd70cbea571e838220cc53b466"
+  version "1.3.1"
+  sha256 "3dbaee07f5534ab1dfae7487d41e89e4e8d776a4de4241b1ac00a38561ba1685"
 
   url "https://github.com/egowic/KeyboardCleaner/releases/download/v#{version}/KeyboardCleaner.zip"
   name "KeyboardCleaner"
